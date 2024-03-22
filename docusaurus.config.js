5b2b6f79d77b5e1956b97bbedaddd734597b3cd9
maxtermax <esnene02@gmail.com>
2024-03-22 17:37:01 -0500

    Ignore broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,10 +22,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
+  onBrokenLinks: 'ignore',
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
